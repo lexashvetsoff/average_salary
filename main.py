55edc610fc0946a764dfe66f2e2419090eda3f35
Aleksey <lexa.shvetsoff@yandex.ru>
2022-01-17 22:09:47 +0800

    Объеденил файлы + добавил вывод таблицы hh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from terminaltables import AsciiTable
 from pprint import pprint
 
 def predict_rub_salary_hh(vacansy):
@@ -15,59 +16,148 @@ def predict_rub_salary_hh(vacansy):
     else:
         return None
 
-langs = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go', 'Objective-C']
-# langs = ['Python']
 
-url = 'https://api.hh.ru/vacancies'
+def predict_rub_salary_sj(vacancy):
+        if vacancy['currency'] == 'rub':
+            if vacancy['payment_from'] != 0 and vacancy['payment_to'] != 0:
+                return (vacancy['payment_from'] + vacancy['payment_to']) / 2
+            elif vacancy['payment_from'] == 0 and vacancy['payment_to'] != 0:
+                return vacancy['payment_to'] * 0.8
+            elif vacancy['payment_from'] != 0 and vacancy['payment_to'] == 0:
+                return  vacancy['payment_from'] * 1.2
+            else:
+                return None
+        else:
+            return None
 
-rating = {}
 
-for lang in langs:
-    full_vacansyes = []
+def get_count_pages(total, count):
+    if total % count != 0:
+        return (total // count) + 1
+    else:
+        return total // count
 
-    pages = 1
-    page = 0
-    while page < pages:
-        payload = {
-            'text': f'Программист {lang}',
-            'area': 1,
-            'only_with_salary': True,
-            'period': 30,
-            'per_page': 100,  # количество вакансий на странице
-            'page': page
-        }
 
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
+def print_table(data, title, titles):
+    table_data = []
+    table_data.append(titles)
+    
+    for item in data:
+        row_table = [item, data[item]['vacancies_found'], data[item]['vacancies_processed'], data[item]['average_salary']]
+        table_data.append(row_table)
 
-        answer = response.json()
-        pages = answer['pages']
-        page = page + 1
-        # print(answer)
+    table = AsciiTable(table_data, title)
+    table.justify_columns[4] = 'right'
+    print(table.table)
 
-        vacansyes = answer['items']
-        # print(type(vacansyes))
-        full_vacansyes.extend(vacansyes)
 
-    # print(type(full_vacansyes), '+++')
-    print(pages, lang)
+def requests_hh(langs, url):
+    rating = {}
 
-    vacancies_processed = 0
-    sum = 0
+    for lang in langs:
+        full_vacansyes = []
 
-    for vacansy in full_vacansyes:
-        # print(predict_rub_salary(vacansy['salary']))
-        if vacansy['salary'] is not None:
-            salary = predict_rub_salary_hh(vacansy['salary'])
+        pages = 1
+        page = 0
+        while page < pages:
+            payload = {
+                'text': f'Программист {lang}',
+                'area': 1,
+                'only_with_salary': True,
+                'period': 30,
+                'per_page': 100,  # количество вакансий на странице
+                'page': page
+            }
+
+            response = requests.get(url, params=payload)
+            response.raise_for_status()
+
+            answer = response.json()
+            pages = answer['pages']
+            page = page + 1
+
+            vacansyes = answer['items']
+            full_vacansyes.extend(vacansyes)
+
+        vacancies_processed = 0
+        sum = 0
+
+        for vacansy in full_vacansyes:
+            if vacansy['salary'] is not None:
+                salary = predict_rub_salary_hh(vacansy['salary'])
+                if salary is not None:
+                    sum = sum + salary
+                    vacancies_processed = vacancies_processed + 1
+
+        average_salary = int(sum / vacancies_processed)
+
+        rating[lang] = {"vacancies_found": answer['found'], 
+                        'vacancies_processed': vacancies_processed, 
+                        'average_salary': average_salary}
+    return rating
+
+
+def requests_sj(langs, url):
+    rating = {}
+
+    for lang in langs:
+        full_vacansyes = []
+
+        pages = 1
+        page = 0
+        count = 50
+        while page < pages:
+            headers = {
+                'X-Api-App-Id': 'v3.r.121869903.b04be04a127192d565b8e50e88de28c1d40457f4.dec3f4bbad8bbf19cdc9a2312447cdc52252fb18'
+            }
+
+            payload = {
+                'keyword': lang,
+                'period': 0,
+                'town': 'Москва',
+                'catalogues': 'Разработка, программирование',
+                'page': page,
+                'count': count
+            }
+
+            response = requests.get(url, headers=headers, params=payload)
+            response.raise_for_status()
+
+            answer = response.json()
+            pages = get_count_pages(answer['total'], count)
+            page = page + 1
+
+            vacansyes = answer['objects']
+            full_vacansyes.extend(vacansyes)
+
+        vacancies_processed = 0
+        sum = 0
+
+        for vacansy in full_vacansyes:
+            salary = predict_rub_salary_sj(vacansy)
             if salary is not None:
                 sum = sum + salary
                 vacancies_processed = vacancies_processed + 1
 
-    average_salary = int(sum / vacancies_processed)
+        average_salary = int(sum / vacancies_processed)
 
-    rating[lang] = {"vacancies_found": answer['found'], 
-                    'vacancies_processed': vacancies_processed, 
-                    'average_salary': average_salary}
+        rating[lang] = {"vacancies_found": answer['total'], 
+                        'vacancies_processed': vacancies_processed, 
+                        'average_salary': average_salary}
+    return rating
 
-pprint(rating)
-# pprint(full_vacansyes)
+
+langs = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go', 'Objective-C']
+
+url_hh = 'https://api.hh.ru/vacancies'
+url_sj = 'https://api.superjob.ru/2.0/vacancies/'
+
+rating_hh = requests_hh(langs, url_hh)
+rating_sj = requests_sj(langs, url_sj)
+
+title_hh = 'HeadHanter Moscow'
+title_sj = 'SuperJob Moscow'
+titles = ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+
+print_table(rating_hh, title_hh, titles)
+print_table(rating_sj, title_sj, titles)
